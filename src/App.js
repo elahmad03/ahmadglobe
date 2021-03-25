4f6170015283c9331e1076075fd422077f30756b
elahmad <elahmad95@gmail.com>
2021-03-25 22:41:04 +0100

    deleted app.js and app.css components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './Navbar';
+import Project from './Project';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Footer from './Footer';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="/Project">
+          <Project />
+        </Route>
+      </Switch>     
     </div>
+    <Footer />
+    </Router>
+    
   );
 }
 
